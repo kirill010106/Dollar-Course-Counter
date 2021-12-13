4ec2bc761e5e9c4dcf2933da8b5d1030e5008b2e
Kirill <konstruktor.png22@mail.ru>
2021-12-13 23:12:04 +0300

    optimize

--- converter.py
+++ converter.py
@@ -1,7 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import random
-import time
 def get_course():
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"
